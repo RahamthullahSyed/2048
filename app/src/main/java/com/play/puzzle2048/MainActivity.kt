c1cc92c9de02fe3edbdab55d709644a34cd28024
Rahamthullah AEE <rahamthullahsyed@gmail.com>
2026-05-22 16:31:07 +0530

    splash screen.

--- app/src/main/java/com/play/puzzle2048/MainActivity.kt
+++ app/src/main/java/com/play/puzzle2048/MainActivity.kt
@@ -213,6 +213,13 @@ class MainActivity : ComponentActivity() {
                 val barThickness = screenHeight * (20f / 640f)
 
                 if (!isWebViewReady) {
+                    Image(
+                        painter = painterResource(id = R.drawable.splash_screen),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.FillBounds
+                    )
+
                     val animatedProgress by animateFloatAsState(
                         targetValue = webViewProgress,
                         animationSpec = tween(durationMillis = 800), // Predictable duration
